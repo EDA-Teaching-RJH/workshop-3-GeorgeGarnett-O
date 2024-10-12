caa88675aac8208e69e4b55a52f5de84bc0211a1
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-12 11:37:31 +0000

    score grader

--- partThree.py
+++ partThree.py
@@ -1,4 +1,24 @@
 score=int (input ("an number bewtween 0 and 100"))
 
+print ("your grade is")
 
+if score >= 90: 
+
+  print ("A")
+
+elif score >= 80: 
+
+  print ("B")
+
+elif score >= 60:
+
+  print ("c")
+
+elif score >= 40:
+
+  print ("D")
+
+elif score >= 20:
+
+  print ("f")
 
