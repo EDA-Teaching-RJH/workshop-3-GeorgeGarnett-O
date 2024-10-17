ff0c58b3a8afebc510ccfc5a3f2af064522e807f
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-17 13:51:22 +0000

    pricing for uni

--- partSix.py
+++ partSix.py
@@ -1,1 +1,18 @@
+age = int(input("age of user"))
+
+freshman= input(input("are you a uni student (yes/no): "))
+
+if age < 12 or age >= 30: 
+    price = 15 
+
+elif 12 <= age <= 35 and freshman == ("yes"):
+
+     price = 12 
+
+elif 12 <= age <= 35 and freshman == ("no"):
+
+ price = 24
+
+
+
 
