c245979ff65afacd2389dc3998055e81d111991d
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-17 13:00:25 +0000

    password and username login

--- partFour.py
+++ partFour.py
@@ -1,1 +1,22 @@
+corect_username = "novice" 
 
+correct_password = "123"
+
+
+username = input("username login")
+
+password = input("password login")
+
+if username == corect_username and password == correct_password:
+
+  print ("access granted")
+
+  print ("welcome sir")
+
+else:  
+
+  print ("access denied") 
+
+  print ("try again later")
+
+  
