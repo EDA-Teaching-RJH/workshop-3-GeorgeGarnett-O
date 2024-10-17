f84ee492ff57a917719dd6c4897d5587f89354c2
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-17 13:16:11 +0000

    week days

--- partFive.py
+++ partFive.py
@@ -1,1 +1,10 @@
+day=(input("type a day found within the week!"))
+
+if day == ("satuday") or day == ("sunday"):
+    print ("still gotta work on this day ")
+    print ("its not over yet")
+
+else:
+    print ("its still a work day")
+    print ( "no rest for the wicked")
 
